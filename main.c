40b8da43ab38ab02c57d216ff16d8011a3edb47e
st1x51 <st1x.bc2@gmail.com>
2020-12-22 18:42:12 +0300

    free libc heap
    thx crow_bar

--- main.c
+++ main.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 PSP_MODULE_INFO("progs", PSP_MODULE_USER, 1, 0);
-void Com_Printf(char* msg, ...);
+/*void Com_Printf(char* msg, ...);*/
 
+int __psp_free_heap(void);
 int module_start( SceSize arglen, void *argp )
 {
 	return 0;
@@ -11,6 +12,7 @@ int module_start( SceSize arglen, void *argp )
 
 int module_stop( SceSize arglen, void *argp )
 {
-	Com_Printf("PRX Stopped\n");
+	/*Com_Printf("PRX Stopped\n");*/
+	__psp_free_heap(); //libc heap
 	return 0;
 }
